e0c064e4d02e31df2883f910c8df4a0dabb949f4
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2023-07-21 00:58:50 +0700

    add code splitting

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   entry: {
@@ -54,6 +55,7 @@ module.exports = {
         }
       ],
       overrideExtension: true
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ]
 }
